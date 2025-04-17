e3624bac1bddb1f985af89e5ff62d00d977739c5
swot786 <84971494+swot786@users.noreply.github.com>
2025-04-17 15:38:35 +0200

    creating a pwa version of the app

--- script.js
+++ script.js
@@ -334,4 +334,15 @@ searchInputElement.addEventListener(`input`, () => {
   }
 });
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
+
 window.onload = displayEquipmentsFromLocalStorage;
+
+
---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+const cmmsMedicalEquipment = "cmms-medical-equipment-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/table2excel.js",
+  "/images/page_excel.png"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(cmmsMedicalEquipment).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
